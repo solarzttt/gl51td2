936234c3362a20b3d931e955d6c866f950d79a97
solarzttt <tan.zhang@utbm.fr>
2019-04-11 16:32:07 +0200

    first commit

---
+++ src/main/groovy/projet/gl51/Student.groovy
@@ -0,0 +1,6 @@
+package projet.gl51;
+
+public class Student {
+	String firstName
+	String lastName
+}
---
+++ src/main/groovy/projet/gl51/StudentController.groovy
@@ -0,0 +1,15 @@
+package projet.gl51
+
+import io.micronaut.http.HttpStatus
+import io.micronaut.http.annotation.Controller
+import io.micronaut.http.annotation.Get
+
+
+@Controller("/student")
+class StudentController {
+
+    @Get("/")
+    List<Student> index() {
+        [new Student(firstName: "CHEN", lastName: "HUANG")]
+    }
+}
